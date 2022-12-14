cfc929ff5e509eb3c7b50d5ac8e1f1f6d7941bd3
jmgomez <info@jmgomez.me>
2022-12-14 16:56:02 +0000

    adds empty template

---
+++ Source/NimTemplate/NimTemplate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NimTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NimTemplate, "NimTemplate" );
---
+++ Source/NimTemplate/NimTemplateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NimTemplateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIMTEMPLATE_API ANimTemplateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
