6ccefe1d82d5f9b12a5001f83c914b1301cdc5b7
jmgomez <juamangomalv@gmail.com>
2023-01-31 13:32:18 +0000

    bumps plugin

--- Source/NimTemplate/NimTemplate.Build.cs
+++ Source/NimTemplate/NimTemplate.Build.cs
@@ -11,10 +11,9 @@ public class NimTemplate : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
